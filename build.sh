893a4f92ef9635bd5c4451ec2423be95e87b7b0b
Ubuntu <mail4awawa@gmail.com>
2021-01-31 23:45:25 +0100

    Add cache

---
+++ build.sh
@@ -0,0 +1,30 @@
+#/bin/sh
+mkdir ccache
+cd ccache
+wget https://github.com/ccache/ccache/releases/download/v4.1/ccache-4.1.tar.gz
+tar -zxvf ccache-4.1.tar.gz
+cd ccache-4.1
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+if [ "$?" -ne "0" ]; then
+  echo "CMake config failed"
+  exit 1
+fi
+make
+if [ "$?" -ne "0" ]; then
+  echo "Make failed"
+  exit 1
+fi
+make install
+if [ "$?" -ne "0" ]; then
+  echo "Make install failed"
+  exit 1
+fi
+cd ../../..
+rm -rf ccache
+if [ "$?" -ne "0" ]; then
+  echo "Clean up failed"
+  exit 1
+fi
+
