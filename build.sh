c1a00d23db8fb3c7c5d70b7add6804b272805507
Awawa <69086569+awawa-dev@users.noreply.github.com>
2021-02-02 14:11:15 +0100

    Update to ccache 4.2

--- build.sh
+++ build.sh
@@ -1,9 +1,25 @@
 #/bin/sh
 mkdir ccache
 cd ccache
-wget https://github.com/ccache/ccache/releases/download/v4.1/ccache-4.1.tar.gz
-tar -zxvf ccache-4.1.tar.gz
-cd ccache-4.1
+
+wget https://github.com/ccache/ccache/releases/download/v4.2/ccache-4.2.tar.gz
+if [ "$?" -ne "0" ]; then
+  echo "Could not download ccache sources"
+  exit 1
+fi
+
+tar -zxvf ccache-4.2.tar.gz
+if [ "$?" -ne "0" ]; then
+  echo "Extracting of ccache failed"
+  exit 1
+fi
+
+cd ccache-4.2
+if [ "$?" -ne "0" ]; then
+  echo "Missing ccache folder"
+  exit 1
+fi
+
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
