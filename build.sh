1d043a44238455b3164a5de4cfd9fb5d2668d0ba
Awawa <mail4awawa@gmail.com>
2021-09-19 13:16:26 +0200

    Test bullseye

--- build.sh
+++ build.sh
@@ -9,19 +9,19 @@ fi
 mkdir ccache
 cd ccache
 
-wget https://github.com/ccache/ccache/releases/download/v4.3/ccache-4.3.tar.gz
+wget https://github.com/ccache/ccache/releases/download/v4.4.1/ccache-4.4.1.tar.gz
 if [ "$?" -ne "0" ]; then
   echo "Could not download ccache sources"
   exit 1
 fi
 
-tar -zxvf ccache-4.3.tar.gz
+tar -zxvf ccache-4.4.1.tar.gz
 if [ "$?" -ne "0" ]; then
   echo "Extracting of ccache failed"
   exit 1
 fi
 
-cd ccache-4.3
+cd ccache-4.4.1
 if [ "$?" -ne "0" ]; then
   echo "Missing ccache folder"
   exit 1
@@ -29,7 +29,7 @@ fi
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DHIREDIS_FROM_INTERNET=ON -DCMAKE_BUILD_TYPE=Release ..
 if [ "$?" -ne "0" ]; then
   echo "CMake config failed"
   exit 1
