7a31706424f37a4a365bd2b3687da321780f5caf
Awawa <69086569+awawa-dev@users.noreply.github.com>
2025-06-26 23:42:29 +0200

    Migrate all Linux dockers to Qt 6.8 LTS (#4)
    * test
    
    * test 2
    
    * test 3
    
    * test 4
    
    * test 5
    
    * test 6
    
    * test 7
    
    * test 8
    
    * test 9
    
    * test 10
    
    * test 11
    
    * test 12
    
    * test 13
    
    * test 14
    
    * test 15
    
    * test 16
    
    * Test 17
    
    * Prepare
    
    * Update CCache
    
    * Test 18
    
    * Test 19
    
    * Update ubuntu
    
    * Update Fedora 42
    
    * Update build.sh
    
    * test 20
    
    * Update build.sh
    
    * Update push.yml
    
    * Update push.yml
    
    * Update push.yml
    
    * Update push.yml
    
    * Update
    
    * Update push.yml
    
    * use composite
    
    * Update push.yml
    
    * fix
    
    * fix2
    
    * Update push.yml
    
    * Update push.yml
    
    * Update push.yml
    
    * Update push.yml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * fix
    
    * fix

--- build.sh
+++ build.sh
@@ -1,5 +1,106 @@
 #!/bin/bash
 
+# QT 6.8 LTS
+qt_version="6.8.3"
+git clone --branch v${qt_version} https://github.com/qt/qtbase.git qt_lts
+mkdir qt_build
+cd qt_build
+../qt_lts/configure -prefix /usr -bindir /usr/qt_${qt_version}_bin -headerdir /usr/qt_${qt_version}_include -hostdatadir /usr/qt_${qt_version}_host -archdatadir /usr/qt_${qt_version} -datadir /usr/qt_${qt_version} -submodules qtbase,qtnetwork -no-sbom -no-dbus -no-gui -no-widgets -no-sql-sqlite -no-icu -skip qtsql -skip qtxml -nomake tests -nomake examples
+if [ "$?" -ne "0" ]; then
+  echo "Qt configuration failed"
+  exit 1
+fi
+cmake --build . --parallel
+if [ "$?" -ne "0" ]; then
+  echo "Qt build failed"
+  exit 1
+fi
+cmake --install .
+if [ "$?" -ne "0" ]; then
+  echo "Qt install failed"
+  exit 1
+fi
+
+rm -r * .*
+git clone --branch v${qt_version} https://github.com/qt/qtserialport.git qtserialport
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DQT_GENERATE_SBOM=OFF ./qtserialport
+if [ "$?" -ne "0" ]; then
+  echo "Qt serial configuration failed"
+  exit 1
+fi
+cmake --build . --parallel
+if [ "$?" -ne "0" ]; then
+  echo "Qt serialport build failed"
+  exit 1
+fi
+cmake --install .
+if [ "$?" -ne "0" ]; then
+  echo "Qt serialport install failed"
+  exit 1
+fi
+
+cd ..
+rm -r qt_lts
+if [ "$?" -ne "0" ]; then
+  echo "Qt clean failed (1)"
+  exit 1
+fi
+rm -r qt_build
+if [ "$?" -ne "0" ]; then
+  echo "Qt clean failed (2)"
+  exit 1
+fi
+
+# CCACHE
+ccache_version="4.11.3"
+
+mkdir ccache
+cd ccache
+
+wget "https://github.com/ccache/ccache/releases/download/v${ccache_version}/ccache-${ccache_version}.tar.gz"
+if [ "$?" -ne "0" ]; then
+  echo "Could not download ccache sources"
+  exit 1
+fi
+
+tar -zxvf "ccache-${ccache_version}.tar.gz"
+if [ "$?" -ne "0" ]; then
+  echo "Extracting of ccache failed"
+  exit 1
+fi
+
+cd "ccache-${ccache_version}"
+if [ "$?" -ne "0" ]; then
+  echo "Missing ccache folder"
+  exit 1
+fi
+
+mkdir build
+cd build
+cmake -DHIREDIS_FROM_INTERNET=ON -DCMAKE_BUILD_TYPE=Release ..
+if [ "$?" -ne "0" ]; then
+  echo "CMake config failed"
+  exit 1
+fi
+make -j $(nproc)
+if [ "$?" -ne "0" ]; then
+  echo "Make failed"
+  exit 1
+fi
+make install
+if [ "$?" -ne "0" ]; then
+  echo "Make install failed"
+  exit 1
+fi
+cd ../../..
+rm -r ccache
+if [ "$?" -ne "0" ]; then
+  echo "Clean up failed"
+  exit 1
+fi
+
+exit 0
+
 # ICU
 echo '------------------------------------------------- find libicudata -------------------------------------------------'
 for file in `find / -name libicu* -type f,l`; do echo $file; ls -la $file;done
